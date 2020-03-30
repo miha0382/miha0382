820fdc1bef24f70f6be1d3d033fdd873c0f2d51a
Михаил Курбатов <miha038229@gmail.com>
2020-03-30 14:51:43 +0400

    Первый проект

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList4 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList5 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.imageList6 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.Kahns_Kave;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.White;
+            this.imageList1.Images.SetKeyName(0, "Sub-Zero1.1.png");
+            this.imageList1.Images.SetKeyName(1, "Sub-Zero1.2.png");
+            this.imageList1.Images.SetKeyName(2, "Sub-Zero1.3.png");
+            this.imageList1.Images.SetKeyName(3, "Sub-Zero1.4.png");
+            this.imageList1.Images.SetKeyName(4, "Sub-Zero1.5.png");
+            this.imageList1.Images.SetKeyName(5, "Sub-Zero1.6.png");
+            this.imageList1.Images.SetKeyName(6, "Sub-Zero1.7.png");
+            this.imageList1.Images.SetKeyName(7, "Sub-Zero1.6.png");
+            this.imageList1.Images.SetKeyName(8, "Sub-Zero1.5.png");
+            this.imageList1.Images.SetKeyName(9, "Sub-Zero1.4.png");
+            this.imageList1.Images.SetKeyName(10, "Sub-Zero1.3.png");
+            this.imageList1.Images.SetKeyName(11, "Sub-Zero1.2.png");
+            this.imageList1.Images.SetKeyName(12, "Sub-Zero1.1.png");
+            this.imageList1.Images.SetKeyName(13, "Sub-ZeroFalling1.png");
+            this.imageList1.Images.SetKeyName(14, "Sub-ZeroFalling2.png");
+            this.imageList1.Images.SetKeyName(15, "Sub-ZeroFalling3.png");
+            this.imageList1.Images.SetKeyName(16, "Sub-ZeroFalling4.png");
+            this.imageList1.Images.SetKeyName(17, "Sub-ZeroFalling5.png");
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Algerian", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(278, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Fight";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.White;
+            this.imageList2.Images.SetKeyName(0, "Scorpion1.png");
+            this.imageList2.Images.SetKeyName(1, "Scorpion2.png");
+            this.imageList2.Images.SetKeyName(2, "Scorpion3.png");
+            this.imageList2.Images.SetKeyName(3, "Scorpion4.png");
+            this.imageList2.Images.SetKeyName(4, "Scorpion3.png");
+            this.imageList2.Images.SetKeyName(5, "Scorpion2.png");
+            this.imageList2.Images.SetKeyName(6, "Scorpion1.png");
+            this.imageList2.Images.SetKeyName(7, "Scorpion5.png");
+            this.imageList2.Images.SetKeyName(8, "Scorpion6.png");
+            this.imageList2.Images.SetKeyName(9, "Scorpion7.png");
+            this.imageList2.Images.SetKeyName(10, "Scorpion8.png");
+            this.imageList2.Images.SetKeyName(11, "Scorpion9.png");
+            this.imageList2.Images.SetKeyName(12, "Scorpion10.png");
+            this.imageList2.Images.SetKeyName(13, "Scorpion11.png");
+            this.imageList2.Images.SetKeyName(14, "Scorpion12.png");
+            this.imageList2.Images.SetKeyName(15, "Scorpion1.png");
+            this.imageList2.Images.SetKeyName(16, "ScorpionWin1.png");
+            this.imageList2.Images.SetKeyName(17, "ScorpionWin2.png");
+            this.imageList2.Images.SetKeyName(18, "ScorpionWin3.png");
+            this.imageList2.Images.SetKeyName(19, "ScorpionWin4.png");
+            // 
+            // imageList3
+            // 
+            this.imageList3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList3.ImageStream")));
+            this.imageList3.TransparentColor = System.Drawing.Color.White;
+            this.imageList3.Images.SetKeyName(0, "ScorpionWalking9.png");
+            this.imageList3.Images.SetKeyName(1, "ScorpionWalking8.png");
+            this.imageList3.Images.SetKeyName(2, "ScorpionWalking7.png");
+            this.imageList3.Images.SetKeyName(3, "ScorpionWalking6.png");
+            this.imageList3.Images.SetKeyName(4, "ScorpionWalking5.png");
+            this.imageList3.Images.SetKeyName(5, "ScorpionWalking4.png");
+            this.imageList3.Images.SetKeyName(6, "ScorpionWalking3.png");
+            this.imageList3.Images.SetKeyName(7, "ScorpionWalking2.png");
+            this.imageList3.Images.SetKeyName(8, "ScorpionWalking1.png");
+            // 
+            // imageList4
+            // 
+            this.imageList4.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList4.ImageStream")));
+            this.imageList4.TransparentColor = System.Drawing.Color.White;
+            this.imageList4.Images.SetKeyName(0, "Sub-Zero2.1.png");
+            this.imageList4.Images.SetKeyName(1, "Sub-Zero2.2.png");
+            this.imageList4.Images.SetKeyName(2, "Sub-Zero2.3.png");
+            this.imageList4.Images.SetKeyName(3, "Sub-Zero2.4.png");
+            this.imageList4.Images.SetKeyName(4, "Sub-Zero2.5.png");
+            this.imageList4.Images.SetKeyName(5, "Sub-Zero2.6.png");
+            this.imageList4.Images.SetKeyName(6, "Sub-Zero2.7.png");
+            this.imageList4.Images.SetKeyName(7, "Sub-Zero2.8.png");
+            this.imageList4.Images.SetKeyName(8, "Sub-Zero2.9.png");
+            // 
+            // imageList5
+            // 
+            this.imageList5.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList5.ImageStream")));
+            this.imageList5.TransparentColor = System.Drawing.Color.White;
+            this.imageList5.Images.SetKeyName(0, "Sub-ZeroFalling6.png");
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 80);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(730, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 80);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Red;
+            this.pictureBox4.Location = new System.Drawing.Point(474, 58);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(256, 22);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // imageList6
+            // 
+            this.imageList6.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList6.ImageStream")));
+            this.imageList6.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList6.Images.SetKeyName(0, "Безымянный1.png");
+            this.imageList6.Images.SetKeyName(1, "Безымянный2.png");
+            this.imageList6.Images.SetKeyName(2, "Безымянный3.png");
+            this.imageList6.Images.SetKeyName(3, "Безымянный4.png");
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(596, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(128, 35);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Assembler";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Blue;
+            this.textBox2.Location = new System.Drawing.Point(76, 17);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(128, 35);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Pervak";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox3.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Red;
+            this.textBox3.Location = new System.Drawing.Point(278, 110);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(237, 91);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Assembler\r\n Wins";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Visible = false;
+            //this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList3;
+        private System.Windows.Forms.ImageList imageList4;
+        private System.Windows.Forms.ImageList imageList5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.ImageList imageList6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap buf;
+        int stage1 = 0, stage2 = 0, stage3 = 0, stage4 = 0, stage5 = 0, stage6 = 0;
+        int x1 = 130, y1 = 100, x2 = 350, y2 = 100, x3 = 220;
+        int k = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            buf = new Bitmap(Properties.Resources.Kahns_Kave);//Определение буфера
+            g = Graphics.FromImage(buf);//Определение графики из буфера
+            imageList1.Draw(g, new Point(x1, y1), stage1);
+            imageList2.Draw(g, new Point(x2, y2), stage2);
+            imageList6.Draw(g, new Point(45, 28), stage6);
+            pictureBox1.Image = buf;
+        }      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            k++;
+            buf = new Bitmap(Properties.Resources.Kahns_Kave);
+            g = Graphics.FromImage(buf);
+            if (x1 < 211)
+            {
+                imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList4.Draw(g, new Point(x1, y1), stage4);
+                imageList3.Draw(g, new Point(x2, y2), stage3);
+                x1 += 10;
+                x2 -= 10;
+                stage3++;
+                stage4++;                
+            }
+            if (k > 9 && k < 22)
+            {
+                imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList1.Draw(g, new Point(x1, y1), stage1);
+                stage1++;
+                imageList2.Draw(g, new Point(x2, y2), stage2);
+                if (stage2 < 3) stage2++;
+            }
+            if (k > 21 && k < 35)
+            {
+                imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList2.Draw(g, new Point(x2, y2), stage2);
+                if(k < 35) stage2++;
+                if(k < 33) imageList1.Draw(g, new Point(x3, y1), stage1);
+                if (k > 28 && k < 32)
+                {
+                    imageList6.Draw(g, new Point(45, 28), stage6);
+                    stage6++;
+                } 
+                if (k > 27 && k < 33)
+                {                    
+                    stage1++;
+                    x3 -= 20;
+                }
+                if (k > 32) imageList5.Draw(g, new Point(x3, y1 + 70), stage5);
+            }
+            if(k > 34 && k < 39)
+            {
+                imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList5.Draw(g, new Point(x3, y1 + 70), stage5);
+                imageList2.Draw(g, new Point(x2, y2), stage2);
+                stage2++;
+            }      
+            if(k < 39) pictureBox1.Image = buf;
+            if(k == 39) textBox3.Visible = true;
+        }
+       
+        Boolean bl = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = bl;
+            bl = !bl;
+            timer1.Enabled = bl;           
+        }
+    }
+}
