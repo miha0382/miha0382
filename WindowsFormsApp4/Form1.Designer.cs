2185b65096eb13bafbd666d0d001f28a04c8a996
Михаил Курбатов <miha038229@gmail.com>
2020-03-30 15:14:14 +0400

    Добавилась возможность выбора исхода "сражения". Побеждает тот персонаж, на чьё имя нажал пользователь, запуская анимацию.

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -40,15 +40,18 @@
             this.imageList5 = new System.Windows.Forms.ImageList(this.components);
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.imageList6 = new System.Windows.Forms.ImageList(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.imageList7 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList8 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList9 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList10 = new System.Windows.Forms.ImageList(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -93,12 +96,12 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.Font = new System.Drawing.Font("Algerian", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(278, 110);
+            this.button1.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(474, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(237, 91);
+            this.button1.Size = new System.Drawing.Size(256, 32);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Fight";
+            this.button1.Text = "ASSEMBLER";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -181,15 +184,6 @@
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackColor = System.Drawing.Color.Red;
-            this.pictureBox4.Location = new System.Drawing.Point(474, 58);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(256, 22);
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
-            // 
             // imageList6
             // 
             this.imageList6.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList6.ImageStream")));
@@ -199,40 +193,12 @@
             this.imageList6.Images.SetKeyName(2, "Безымянный3.png");
             this.imageList6.Images.SetKeyName(3, "Безымянный4.png");
             // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
-            this.textBox1.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.Red;
-            this.textBox1.Location = new System.Drawing.Point(596, 17);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(128, 35);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.Text = "Assembler";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
-            this.textBox2.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.Color.Blue;
-            this.textBox2.Location = new System.Drawing.Point(76, 17);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(128, 35);
-            this.textBox2.TabIndex = 6;
-            this.textBox2.Text = "Pervak";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox3.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.ForeColor = System.Drawing.Color.Red;
-            this.textBox3.Location = new System.Drawing.Point(278, 110);
+            this.textBox3.Location = new System.Drawing.Point(283, 164);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
@@ -241,17 +207,80 @@
             this.textBox3.Text = "Assembler\r\n Wins";
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox3.Visible = false;
-            //this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // imageList7
+            // 
+            this.imageList7.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList7.ImageStream")));
+            this.imageList7.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList7.Images.SetKeyName(0, "HP1.png");
+            this.imageList7.Images.SetKeyName(1, "HP2.png");
+            this.imageList7.Images.SetKeyName(2, "HP3.png");
+            this.imageList7.Images.SetKeyName(3, "Безымянный4.png");
+            // 
+            // imageList8
+            // 
+            this.imageList8.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList8.ImageStream")));
+            this.imageList8.TransparentColor = System.Drawing.Color.White;
+            this.imageList8.Images.SetKeyName(0, "ScorpionFalling.png");
+            this.imageList8.Images.SetKeyName(1, "ScorpionFalling1.png");
+            this.imageList8.Images.SetKeyName(2, "ScorpionFalling2.png");
+            this.imageList8.Images.SetKeyName(3, "ScorpionFalling3.png");
+            this.imageList8.Images.SetKeyName(4, "ScorpionFalling4.png");
+            // 
+            // imageList9
+            // 
+            this.imageList9.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList9.ImageStream")));
+            this.imageList9.TransparentColor = System.Drawing.Color.White;
+            this.imageList9.Images.SetKeyName(0, "ScorpionFalling5.png");
+            // 
+            // imageList10
+            // 
+            this.imageList10.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList10.ImageStream")));
+            this.imageList10.TransparentColor = System.Drawing.Color.White;
+            this.imageList10.Images.SetKeyName(0, "swin1.png");
+            this.imageList10.Images.SetKeyName(1, "swin2.png");
+            this.imageList10.Images.SetKeyName(2, "swin3.png");
+            this.imageList10.Images.SetKeyName(3, "swin4.png");
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.Font = new System.Drawing.Font("Algerian", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(70, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 32);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "\"PERVAK\"";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(283, 164);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(237, 91);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "\"PERVAK\"\r\n Wins";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
@@ -261,7 +290,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -279,11 +307,15 @@
         private System.Windows.Forms.ImageList imageList5;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.ImageList imageList6;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ImageList imageList7;
+        private System.Windows.Forms.ImageList imageList8;
+        private System.Windows.Forms.ImageList imageList9;
+        private System.Windows.Forms.ImageList imageList10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -8,8 +8,11 @@ namespace WindowsFormsApp4
     {
         Graphics g;
         Bitmap buf;
-        int stage1 = 0, stage2 = 0, stage3 = 0, stage4 = 0, stage5 = 0, stage6 = 0;
+        int stage1 = 0, stage2 = 0, stage3 = 0, stage4 = 0, stage5 = 0, stage6 = 0, stage7 = 0;
         int x1 = 130, y1 = 100, x2 = 350, y2 = 100, x3 = 220;
+
+        
+
         int k = 0;
         public Form1()
         {
@@ -19,8 +22,9 @@ namespace WindowsFormsApp4
             imageList1.Draw(g, new Point(x1, y1), stage1);
             imageList2.Draw(g, new Point(x2, y2), stage2);
             imageList6.Draw(g, new Point(45, 28), stage6);
+            imageList7.Draw(g, new Point(300, 28), stage6);
             pictureBox1.Image = buf;
-        }      
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
             k++;
@@ -29,16 +33,18 @@ namespace WindowsFormsApp4
             if (x1 < 211)
             {
                 imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList7.Draw(g, new Point(300, 28), 0);
                 imageList4.Draw(g, new Point(x1, y1), stage4);
                 imageList3.Draw(g, new Point(x2, y2), stage3);
                 x1 += 10;
                 x2 -= 10;
                 stage3++;
-                stage4++;                
+                stage4++;
             }
             if (k > 9 && k < 22)
             {
                 imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList7.Draw(g, new Point(300, 28), 0);
                 imageList1.Draw(g, new Point(x1, y1), stage1);
                 stage1++;
                 imageList2.Draw(g, new Point(x2, y2), stage2);
@@ -47,38 +53,98 @@ namespace WindowsFormsApp4
             if (k > 21 && k < 35)
             {
                 imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList7.Draw(g, new Point(300, 28), 0);
                 imageList2.Draw(g, new Point(x2, y2), stage2);
-                if(k < 35) stage2++;
-                if(k < 33) imageList1.Draw(g, new Point(x3, y1), stage1);
+                if (k < 35) stage2++;
+                if (k < 33) imageList1.Draw(g, new Point(x3, y1), stage1);
                 if (k > 28 && k < 32)
                 {
                     imageList6.Draw(g, new Point(45, 28), stage6);
                     stage6++;
-                } 
+                }
                 if (k > 27 && k < 33)
-                {                    
+                {
                     stage1++;
                     x3 -= 20;
                 }
                 if (k > 32) imageList5.Draw(g, new Point(x3, y1 + 70), stage5);
             }
-            if(k > 34 && k < 39)
+            if (k > 34 && k < 39)
             {
                 imageList6.Draw(g, new Point(45, 28), stage6);
+                imageList7.Draw(g, new Point(300, 28), 0);
                 imageList5.Draw(g, new Point(x3, y1 + 70), stage5);
                 imageList2.Draw(g, new Point(x2, y2), stage2);
                 stage2++;
-            }      
-            if(k < 39) pictureBox1.Image = buf;
-            if(k == 39) textBox3.Visible = true;
+            }
+            if (k < 39) pictureBox1.Image = buf;
+            if (k == 39) textBox3.Visible = true;
         }
-       
+        
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            k++;
+            buf = new Bitmap(Properties.Resources.Kahns_Kave);
+            g = Graphics.FromImage(buf);
+            if (x1 < 211)
+            {
+                imageList6.Draw(g, new Point(45, 28), 0);
+                imageList7.Draw(g, new Point(300, 28), stage6);
+                imageList4.Draw(g, new Point(x1, y1), stage4);
+                imageList3.Draw(g, new Point(x2, y2), stage3);
+                x1 += 10;
+                x2 -= 10;
+                stage3++;
+                stage4++;
+            }
+            if (k > 9 && k < 30)
+            {
+                imageList6.Draw(g, new Point(45, 28), 0);
+                imageList7.Draw(g, new Point(300, 28), stage6);
+
+                if (k < 25) imageList1.Draw(g, new Point(x1, y1), stage1);
+                if (k < 22) stage1++;
+                if (k < 19) imageList2.Draw(g, new Point(x2, y2), 0);
+
+                if (k > 19 && k < 23)
+                {
+                    imageList7.Draw(g, new Point(300, 28), stage6);
+                    stage6++;
+                }
+
+
+                if (k > 18 && k < 24)
+                {
+                    imageList8.Draw(g, new Point(x2, y2), stage2);
+                    x2 += 20;
+
+                    stage2++;
+                }
+
+                if (k > 23) imageList9.Draw(g, new Point(x2 - 20, y2 + 70), 0);
+                if (k > 24 && k < 29)
+                {
+                    imageList10.Draw(g, new Point(x1, y1), stage7);
+                    stage7++;
+                }
+            }
+
+            if (k < 30) pictureBox1.Image = buf;
+            if (k > 28) imageList10.Draw(g, new Point(x1, y1), 3);
+
+            if (k == 30) textBox1.Visible = true;
+        }
         Boolean bl = false;
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Visible = bl;
             bl = !bl;
-            timer1.Enabled = bl;           
+            timer1.Enabled = bl;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            bl = !bl;
+            timer2.Enabled = bl;
         }
     }
 }
